21ba8415bbdbc5b0a3aa706ae0e63c266996e780
rautiais <rautiainen.e.salla@gmail.com>
2024-04-18 14:12:05 +0300

    Updated schema

--- schema.sql
+++ schema.sql
@@ -7,25 +7,25 @@ DROP TABLE IF EXISTS user_info CASCADE;
 
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY, 
-    username TEXT UNIQUE CHECK(username IS NOT NULL AND length(username) > 2),
+    username TEXT UNIQUE CHECK(username IS NOT NULL AND length(username) > 2 AND length(username) < 20),
     password TEXT CHECK (password IS NOT NULL AND length(password) > 7)
 );
 
 CREATE TABLE IF NOT EXISTS events (
     id SERIAL PRIMARY KEY,
-    event_name TEXT UNIQUE CHECK(event_name IS NOT NULL AND length(event_name) > 2)
+    event_name TEXT UNIQUE CHECK(event_name IS NOT NULL AND length(event_name) > 2 AND length(event_name) < 20)
 );
 
 CREATE TABLE IF NOT EXISTS event_cat (
     id SERIAL PRIMARY KEY,
-    event_cat_name TEXT UNIQUE CHECK(event_cat_name IS NOT NULL AND length(event_cat_name) > 2),
+    event_cat_name TEXT UNIQUE CHECK(event_cat_name IS NOT NULL AND length(event_cat_name) > 2 AND length(event_cat_name) < 20),
     events_id INTEGER REFERENCES events
 );
 
 CREATE TABLE IF NOT EXISTS groups (
     id SERIAL PRIMARY KEY,
     member_id INTEGER REFERENCES users,
-    group_name TEXT UNIQUE CHECK(group_name IS NOT NULL AND length(group_name) > 2)
+    group_name TEXT UNIQUE CHECK(group_name IS NOT NULL AND length(group_name) > 2 AND length(group_name) < 20)
 );
 
 CREATE TABLE IF NOT EXISTS user_info (
